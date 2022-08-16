eab0c84627bd93a30c5410cc2a956b46590f5385
js061097 <js41074546@gmail.com>
2022-08-16 07:22:27 -0700

    Blocking parameters and printf changes

---
+++ examples/exp.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+export LD_LIBRARY_PATH=/opt/intel-gpu/gmmlib/lib64/:/opt/intel-gpu/igc/lib64/:/opt/intel-gpu/neo/lib64/:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=./../build/:$LD_LIBRARY_PATH 
+
